49c492222605a7c3f7b1a12a3ffa6233728626c0
andreza <andreza.21pessoa@gmail.com>
2022-02-12 21:22:38 -0300

    adicionando o card na tabela

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,6 +5,7 @@ import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {MatTableModule} from '@angular/material/table';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class CoursesModule { }
