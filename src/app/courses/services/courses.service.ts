2f5ef35eaa57fecbb7e08629701decc568c0c2b0
andreza <andreza.21pessoa@gmail.com>
2022-02-13 18:13:23 -0300

    chamada Http  Get

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  //CAMINHO DO ENDPOINT
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'angular', category: 'front-end'},
-      { _id: '2', name: 'java', category: 'back-end'}
-    ];
+  //retorna um observable
+  // pipe faz uma manipulação dos dados
+  // first primeira resposta
+  list(){
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
